45e587786e8908b1aab37c6b7b36e42c32fad5d8
MarinoFajardo <marinofajardo@correo.ugr.es>
2024-02-09 12:44:40 +0100

    "Creación de las Rutas"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { Routes, provideRouter, withDebugTracing } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ResgistroComponent } from './componentes/resgistro/resgistro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClientesComponent } from './componentes/editar-clientes/editar-clientes.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component:TableroComponent},
+    {path: 'login',component:LoginComponent},
+    {path: 'registro',component:ResgistroComponent},
+    {path: 'configuracion',component:ConfiguracionComponent},
+    {path: 'cliente/editar/:id',component:EditarClientesComponent},
+    {path: '**',component:NoEncontradoComponent}
+
+];
+
+export const appConfig:ApplicationConfig = {
+    providers:[provideRouter(routes,withDebugTracing())]
+}
