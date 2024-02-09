f6e340f62623bd251b9d25cdd6a014ae14fcadd8
MarinoFajardo <marinofajardo@correo.ugr.es>
2024-02-09 13:20:16 +0100

    "Creación del cabecero y el footer"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,RouterModule],
+  imports: [CommonModule, RouterOutlet,RouterModule,
+  CabeceroComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componentes/cabecero/cabecero.component.ts
+++ src/app/componentes/cabecero/cabecero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cabecero',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './cabecero.component.html',
   styleUrl: './cabecero.component.css'
 })
